624eb8eeabd09fb2f537b96c767033628bda7fa5
Andrey <Andrey@DESKTOP-VKNE8FS>
2022-06-08 22:12:08 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "cartoteca.h"
+
+int main()
+{
+    Student andrey;
+    cin >> andrey;
+    cout << andrey;
+    cout << endl;
+    Student Jan;
+    cin >> Jan;
+    andrey = Jan;
+    cout << andrey << endl;
+}
